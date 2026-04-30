15db89c460b7b67ed7da278b8d01e31ad6366a70
Fernando Gomes <fernandogomesbnu@gmail.com>
2026-04-30 20:47:33 -0300

    Adicionando exercícios Abstracao

---
+++ src/POO/Basico/Abstracao/Exemplo2/Pix.java
@@ -0,0 +1,13 @@
+package POO.Basico.Abstracao.Exemplo2;
+
+public class Pix extends Pagamento {
+
+    public Pix(double valor) {
+        super(valor);
+    }
+
+    @Override
+    public void processar() {
+        System.out.println("Pagamento com PIX!");
+    }
+}
